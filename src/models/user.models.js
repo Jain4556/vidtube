51c4c896d5c176d5be93377dc6225bdfa68d4f3b
Jain4556 <sunil925business@gmail.com>
2026-02-05 13:20:09 +0530

    models-updated

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -1,5 +1,7 @@
-import { request } from "express";
+
 import mongoose, { Schema } from "mongoose";
+import bcrypt from "bcrypt"
+import jwt from "jsonwebtoken"
 
 
 const userSchema = new Schema(
@@ -46,10 +48,50 @@ const userSchema = new Schema(
             type: String
         }
 
-    }, 
-    {timestamps:  true}
+    },
+    { timestamps: true }
 
 )
+
+userSchema.pre("save", async function (next) {
+    if (!this.modified("password")) return next()
+
+    this.password = bcrypt.hash(this.password, 10)
+
+    next()
+})
+
+
+userSchema.methods.isPasswordCorrect = async function (password) {
+    return await bcrypt.compare(password, this.password)
+    
+}
+
+
+
+userSchema.methods.generateAccessToken = function () {
+    // short lived session
+    return jwt.sign({
+        _id: this._id,
+        email: this.email,
+        username: this.username,
+        fullName: this.fullName,
+    },
+        process.env.ACCESSS_TOKEN_SECRET,
+        { expiresIn: process.env.ACCESSS_TOKEN_EXPIRY }
+    );
+}
+
+userSchema.methods.generateRefreshToken = function () {
+    // short lived session 
+    return jwt.sign({
+        _id: this._id,
+       
+    },
+        process.env.REFRESH_TOKEN_SECRET,
+        { expiresIn: process.env.REFRESH_TOKEN_EXPIRY }
+    );
+}
 
 
 export const User = mongoose.model("User", userSchema)
